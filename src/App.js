3f3df5f51861e402ef45a9c92c8d812ac3cd7fcc
Alden Davidson <adavidson+github@protonmail.ch>
2020-11-06 07:52:12 -0800

    basic boilerplate

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import PokemonApp from './PokemonApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App-header">
+      <PokemonApp/>
     </div>
   );
 }
 
 export default App;
+
+// app top-level
+// pokemon-app
+// pokemon item
---
+++ src/PokemonApp.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class PokemonApp extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h1>Helllo</h1>
+    );
+  }
+}
+
+export default PokemonApp;
